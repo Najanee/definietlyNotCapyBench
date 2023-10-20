ed39a514769bf7689fa6248ca3360d6f90238b1d
Bartłomiej Opiełka <bartlomiej.opielka@capgemini.com>
2023-10-20 14:20:34 +0200

    branch initial commit.

--- Anzeige2/src/main/java/com/capgemini/capybench/Anzeige2/entity/User.java
+++ Anzeige2/src/main/java/com/capgemini/capybench/Anzeige2/entity/User.java
@@ -1,4 +1,18 @@
 package com.capgemini.capybench.Anzeige2.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class User {
+    @Id
+    private long id;
+    @Column
+    private String name;
 }
