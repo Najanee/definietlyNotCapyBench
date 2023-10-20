f1824f7b2daad6b575a267103d730099c2d210bf
Adrian Holubecki <adrian.holubecki@capgemini.com>
2023-10-20 17:09:40 +0200

    very important changes:

--- Anzeige2/src/main/java/com/capgemini/capybench/Anzeige2/controllers/TopicController.java
+++ Anzeige2/src/main/java/com/capgemini/capybench/Anzeige2/controllers/TopicController.java
@@ -1,17 +1,32 @@
 package com.capgemini.capybench.Anzeige2.controllers;
 
+import com.capgemini.capybench.Anzeige2.entity.Person;
+import com.capgemini.capybench.Anzeige2.entity.Subtopic;
 import com.capgemini.capybench.Anzeige2.entity.Topic;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/topic")
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/topic")
 public class TopicController {
 
     @GetMapping("/{id}")
-    public ResponseEntity<Topic> getTopic(@PathVariable("id") int topicId){
-        
-        return ResponseEntity.ok(new Topic());
+    public ResponseEntity<Topic> getTopic(@PathVariable("id") long topicId){
+
+        Topic topic = new Topic(topicId, "Hello Dupa", new HashSet<Person>(), new HashSet<Subtopic>());
+        return ResponseEntity.ok(topic);
+    }
+    @GetMapping
+    public ResponseEntity<List<Topic>> getAllTopics(){
+
+        return ResponseEntity.ok(new ArrayList<>());
     }
 }
---
+++ Anzeige2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO person(id, name) VALUES(1, 'Bartek');
+INSERT INTO person(id, name) VALUES(2, 'Opielka');
+INSERT INTO person(id, name) VALUES(3, 'Bartlomiej');
+INSERT INTO person(id, name) VALUES(4, 'Bartek 2');
+INSERT INTO person(id, name) VALUES(5, 'Bartosz');
